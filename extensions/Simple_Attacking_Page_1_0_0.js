966695edf7eedee6d9363d5fb1c9a85c0a7d5a88
mirror <844912709@qq.com>
2022-04-30 22:57:42 +0800

    Simple_Attacking_Page_1_0_0

---
+++ extensions/Simple_Attacking_Page_1_0_0.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Simple Attacking Page
+// @namespace    TornExtensions
+// @version      1.0
+// @description  屏蔽进攻页面的人物形象
+// @author       htys [1545351]
+// @match        https://www.torn.com/loader.php*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+    const $ = window.jQuery;
+	const intervalID = setInterval(updateUI, 1000);
+	function updateUI(){
+		const attacker = $("div#attacker").find("div[class^='modelLayers___']");
+		attacker.find("div[class^='model___']").remove();
+		attacker.find("div[class^='armoursWrap___']").remove();
+		const defender = $("div#defender").find("div[class^='modelLayers___']");
+		defender.find("div[class^='model___']").remove();
+		defender.find("div[class^='armoursWrap___']").remove();
+	}
+})();
